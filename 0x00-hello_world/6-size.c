a54d661f2b3128bd75a9b009857bf0b246775c80
amiragalal98 <116448908+amiragalal98@users.noreply.github.com>
2023-03-16 19:08:58 +0200

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+
 /**
 * main - Entry point
 *
@@ -13,6 +14,7 @@ int main(void)
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	
 	/*return 0 mean success */
 	
 	return (0);
