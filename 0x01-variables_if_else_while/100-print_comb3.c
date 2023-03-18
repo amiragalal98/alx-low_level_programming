70edf202459b38f649d5554c3e955f4f19714080
amiragalal98 <amg18698@gmail.com>
2023-03-18 03:11:20 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,4 +10,26 @@ int main(void)
 
 	i = 48;
 	e = 48;
-
+	while (e < 58)
+	{
+		i = 48;
+		while (i < 58)
+		{
+			if (e != i && e < i)
+			{
+				putchar(e);
+				putchar(i);
+				if (i == 57 && e == 56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			i++;
+		}
+		e++
+	}
+	putchar('\n');
+	return (0);
+}
