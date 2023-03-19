2ecf7f0be9698982dde63dc1daffb8e71190ada9
amiragalal98 <amg18698@gmail.com>
2023-03-19 05:46:35 -0700

    Clean_Code

---
+++ test/comment_escap.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("comment and escape sequence\n"); /* this is comment allowed */
+	printf("in betty style you can not use // to comment but use /* */\n");
+	return (0);
+/**
+ * escape sequence = character combination consist of a backslash
+ *  followed by a letter or combination of digit
+ *  they specify actions within a line or string of text.
+ *  \n = new line
+ *  \t = tab\
+ *  \v = vertical tab
+ *  the
+ */
+}
