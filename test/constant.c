791281f32ea1a559f6e2bbeca7d27c14de034405
amiragalal98 <amg18698@gmail.com>
2023-03-20 15:06:01 -0700

    constant in C

---
+++ test/constant.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+/**
+ * constant = fixed value
+ * that can not be altered by
+ * the program during its execution
+ */
+	const float pi = 3.14;
+
+	printf("pi equal: %.2f", pi);
+	return (0);
+}
