502409557b4cbe777bb4f1bec7ef161d58e7eacc
amiragalal98 <amg18698@gmail.com>
2023-03-21 05:07:03 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	int str[] = {111, 112, 113, 43, 44, 66, 21, 34};
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
 	int count, size;
 
 	size = sizeof(str) / sizeof(int);
