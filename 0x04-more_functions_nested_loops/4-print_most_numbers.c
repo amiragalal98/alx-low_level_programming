564e42306d6e601993b5f39899eb3af88b346141
amiragalal98 <amg18698@gmail.com>
2023-03-23 05:57:13 -0700

    print number between (0-9)except 2&4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers - Print most number
+ * between (0-9)
+ * but no print 2 and 4
+ * Return: no return
+ */
+void print_most_numbers(void)
+{
+	int ch;
+
+	for (ch = 48; ch < 58; ch++)
+	{
+		if (ch != 50 && ch != 52)
+			_putchar(ch);
+	}
+	_putchar('\n');
+}
