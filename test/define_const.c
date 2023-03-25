47b9548af9e02ca618ada4aa89594e30327d4617
amiragalal98 <amg18698@gmail.com>
2023-03-25 02:23:05 -0700

    How define constant value

---
+++ test/define_const.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#define PI 3.14
+/**
+ * there are two way
+ * to define constant value
+ * define
+ * const
+ * define constant using define
+ * const to define constant
+ * Main -  Entry point
+ * Return: return 0 (success)
+ */
+int main(void)
+{
+	const int i = 0;
+
+	printf("the value of pi is: %.2f\n", PI);
+	printf("the value of i is: %d", i);
+	return (0);
+}
+
+
